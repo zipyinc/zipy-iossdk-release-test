131a0e64bf9128fd69dfb945cb83cce1f95e8b2c
sandeshmurdia <sandeshmurdia123@gmail.com>
2025-04-09 18:46:55 +0530

    changes

--- Package.swift
+++ Package.swift
@@ -16,16 +16,9 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.25.0"),
     ],
     targets: [
-        .target(
-            name: "ZipyiOS",
-            dependencies: [
-                .product(name: "SwiftProtobuf", package: "swift-protobuf"),
-                "ZipyiOSBinary"
-            ]
-        ),
         .binaryTarget(
-            name: "ZipyiOSBinary",
-            url: "https://raw.githubusercontent.com/zipyinc/zipy-iossdk-release-test/0.0.16/ZipyiOS.xcframework.zip",
+            name: "ZipyiOS",
+            url: "https://raw.githubusercontent.com/zipyinc/zipy-iossdk-release-test/0.0.21/ZipyiOS.xcframework.zip",
             checksum: "a5a9824c13aa37fa4b048254a0bcdda10bb742ef856023a926829f6230317651"
         )
     ]
