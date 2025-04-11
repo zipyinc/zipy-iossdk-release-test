5b398afbc30ab08b99d0cd6a77c8fcf8b08a7b22
sandeshmurdia <sandeshmurdia123@gmail.com>
2025-04-11 13:40:36 +0530

    Release version 4.0.1

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let repositoryName = "zipy-iossdk-release-test"
 
 // Package configuration
 let packageName = "ZipyiOS"
-let zipySDKVersion = "4.0.0"
+let zipySDKVersion = "4.0.1"
 
 // Dependencies configuration
 let swiftProtobufVersion = "1.25.0"
@@ -18,7 +18,7 @@ let swiftProtobufPackage = "https://github.com/apple/swift-protobuf.git"
 let binaryTargetName = "ZipyiOS"
 let mainTargetName = "ZipyiOSMain"
 let binaryURL = "https://raw.githubusercontent.com/\(organizationName)/\(repositoryName)/\(zipySDKVersion)/ZipyiOS.xcframework.zip"
-let binaryChecksum = "fba71104c111960ead3313de53657168e122c33c0b082fd074f5c9a9afdd832c"
+let binaryChecksum = "74eacf1ae91eff736ce91df60d87f088054c2cd4e5669d1fe38909a9a9ae6b15"
 
 let package = Package(
     name: packageName,
